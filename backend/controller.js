171a2657860f8c011e3dbbe05d409a23618b6cb0
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-08-07 16:00:21 -0600

    add environment variable to test database creation

--- backend/controller.js
+++ backend/controller.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./todo-database.sqlite');
+process.env.DATABASE_CREATED = 'true';
 
 exports.getTodos = async (event, context) => {
   return new Promise((resolve, reject) => {
