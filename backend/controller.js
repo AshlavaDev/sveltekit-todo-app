a55bf0b17ad84269e73c7fc654424c90adb7a388
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-08-07 15:33:58 -0600

    update database path

--- backend/controller.js
+++ backend/controller.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('todo-database.sqlite');
+const db = new sqlite3.Database('./todo-database.sqlite');
 
 exports.getTodos = async (event, context) => {
   return new Promise((resolve, reject) => {
