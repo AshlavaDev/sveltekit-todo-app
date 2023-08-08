35c8c27873884efe80bdfdde9e4e185444f87b09
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-08-07 21:14:53 -0600

    added permission just in case

--- backend/netlify/functions/controller.js
+++ backend/netlify/functions/controller.js
@@ -2,7 +2,7 @@ const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 
 const dbPath = path.resolve(__dirname, './../../todoDatabase.db');
-const db = new sqlite3.Database(dbPath, (err) => {
+const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
   } else {
