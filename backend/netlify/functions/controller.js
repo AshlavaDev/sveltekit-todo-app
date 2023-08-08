2a5545885c1477f2b5301426c4f6b300975a654b
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-08-07 20:09:10 -0600

    add error checking in db connection

--- backend/netlify/functions/controller.js
+++ backend/netlify/functions/controller.js
@@ -1,7 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
-console.log("Get database");
-const db = new sqlite3.Database('./../../tododatabase.sqlite');
-console.log("Got database");
+const db = new sqlite3.Database('./../../tododatabase.sqlite', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the todos database.');
+});
 
 exports.getTodos = async () => {
   
